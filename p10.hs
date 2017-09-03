44f3ba52060e17056bb208883429bfa4030923c2
Ajit Nath <nath.ajit01@gmail.com>
2017-09-03 21:43:53 +0800

    problem 10

--- p10.hs
+++ p10.hs
@@ -2,13 +2,15 @@
 -- Consecutive duplicates of elements are encoded as lists (N E) 
 -- where N is the number of duplicates of the element E.
 
+packList :: (Eq a) => [a] -> [[a]]
+packList [] = []
+packList [x] = [[x]]
+packList (x:xs) = if x `elem` (head (packList xs))
+                  then (x: (head (packList xs)))  : (tail $ packList xs)
+                  else [x]: (packList xs)
+
+
 runLengthEncode :: (Eq a) => [a] -> [(Int, a)]
-
-runLengthEncode [] = []
-runLengthEncode [(1, a)] = [(1, a)]
-runLengthEncode (x:xs) = 
-        if x == head runLengthEncode xs
-        then (x:(head $ runLengthEncode xs)) : tail runLengthEncode xs
-        else x: runLengthEncode xs
-
--- WIP --
+runLengthEncode xs = 
+        map encode $ packList xs
+        where encode list = (length list, head list) 
