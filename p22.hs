5f908bf1aedc07316be35a6bdccd4177d36b5371
Ajit Nath <nath.ajit01@gmail.com>
2017-10-28 01:10:06 +0800

    [Fix] createRange can use negative indices

--- p22.hs
+++ p22.hs
@@ -1,9 +1,8 @@
  -- Create a list containing integers
 
- createRange :: Int -> Int -> [Int]
+createRange :: Int -> Int -> [Int]
 
 createRange a b
-    | a > b = []
+    | a == b = [a]
+    | a > b = [a] ++ createRange (a - 1) b
     | otherwise = [a] ++ createRange (a + 1) b 
-
--- TODO: Include negative indices 
