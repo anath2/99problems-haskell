0e9fbd62812dc3d4fe4cc724cf1175ae8a5f1b51
Ajit Nath <nath.ajit01@gmail.com>
2017-10-29 19:55:08 +0800

    Select random elements from a list

---
+++ p23.hs
@@ -0,0 +1,13 @@
+-- Return a number of random elements from an array
+
+import System.Random 
+import Control.Monad (replicateM)
+
+getRandnElements :: [a] -> Int -> IO [a]
+getRandnElements [] _ = return []
+getRandnElements xs n
+    | n < 0 = return []
+    | otherwise = do pos <- replicateM n $ getStdRandom $ randomR (0, (length xs) -1)
+                     return [xs !! p | p <- pos]
+    
+-- TODO Select elements without repeating
