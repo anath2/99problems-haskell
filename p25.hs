d3daf33e6fe25b9fce3a5dcb0d556184eab8c27a
Ajit Nath <nath.ajit01@gmail.com>
2017-11-07 12:46:10 +0800

    Generate random permutations of a list

--- p25.hs
+++ p25.hs
@@ -1,12 +1,16 @@
 -- Generate random permutations of a list
+
 import System.Random
 import Control.Monad (replicateM)
 
-rnd_select xs n 
-    | n < 0     = error "N must be greater than zero."
-    | otherwise = replicateM n rand
-        where rand = do r <- randomRIO (0, (length xs) - 1)
-                        return (xs!!r)
+rndSelect :: [a] -> IO [a]
+rndSelect [] = return []
+rndSelect xs = do 
+                ind <- randomRIO (0, (length xs) - 1)
+                let remaining = take ind xs ++ drop (ind + 1) xs                         
+                rest <- rndSelect remaining
+                return ((xs!!ind) : rest)
+
 
 
         
