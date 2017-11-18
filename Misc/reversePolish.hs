bfe2fe3df2ee66a53b8ad76abbad4be725fba7f4
Ajit Nath <nath.ajit01@gmail.com>
2017-11-19 00:00:39 +0800

    Calculate reverse polish notation

--- Misc/reversePolish.hs
+++ Misc/reversePolish.hs
@@ -1,9 +1,13 @@
 -- Solve an expression expressed in reverse Polish notation
--- Reverse polish notation is a form of postfix notation For example, 
--- the expression 10 - (4 + 3) * 2 can be expressed as 10 4 3 + 2 * -
+import Data.List  
 
-import Data.List
-
-reversePolishNotation :: (Num a) => Str -> a
-reversePolishNotation expression = head (foldl foldingFunction [] (words expression))
---[WIP]
+solveRPN :: String -> Float  
+solveRPN = head . foldl foldingFunction [] . words  
+  where   foldingFunction (x:y:ys) "*" = (x * y):ys  
+          foldingFunction (x:y:ys) "+" = (x + y):ys  
+          foldingFunction (x:y:ys) "-" = (y - x):ys  
+          foldingFunction (x:y:ys) "/" = (y / x):ys  
+          foldingFunction (x:y:ys) "^" = (y ** x):ys  
+          foldingFunction (x:xs) "ln" = log x:xs  
+          foldingFunction xs "sum" = [sum xs]  
+          foldingFunction xs numberString = read numberString:xs  
