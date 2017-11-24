ac537fbe66c7e987f35e1054a2307df7bfc9d50a
Ajit Nath <nath.ajit01@gmail.com>
2017-11-25 00:05:02 +0800

    Prime numbers in range

--- Misc/primesInRange.hs
+++ Misc/primesInRange.hs
@@ -2,6 +2,6 @@
 primesInRange :: Int -> [Int]
 primesInRange n 
     | n < 3 = []
-    | otherwise = ...
-
-    
+    | otherwise = [x | x <- [3..(n - 1)], isPrime x ]
+        where
+            isPrime n = null [x | x <- [2..(n-1)], n `mod` x == 0]
