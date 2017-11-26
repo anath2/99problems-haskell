d4e74dfbe76533fb04c94b860a609e8fa9f0ac8a
Ajit Nath <nath.ajit01@gmail.com>
2017-11-26 21:37:15 +0800

    Prime factors with multiples

--- Arithematic/p36.hs
+++ Arithematic/p36.hs
@@ -1,5 +1,17 @@
 -- Get prime factors with their multiplicity
 primeFactors' :: Int -> [(Int, Int)]
 
-primeFactors' n = runLengthEncode . helper
-    where ...
+primeFactors' 1 = []
+primeFactors' n = encode $ helper n 2 
+    where
+        helper 1 _ = []
+        helper n f 
+            | n `mod` f == 0 = f : helper (n `div` f) f
+            | otherwise      = helper n (f + 1)
+        encode = foldr acc []
+            where
+                acc x [] = [(x, 1)]
+                acc x (y@(num, count):ys) 
+                    | x == num = (num, count + 1):ys
+                    | otherwise = (x, 1):y:ys
+        
