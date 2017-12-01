2d9213003c116bdbe90bf6d4aa24ef215e3a9c8d
Ajit Nath <nath.ajit01@gmail.com>
2017-12-01 23:59:22 +0800

    [FIX] Include 2 in the result if the first number is less than or equal to two

--- Arithematic/p39.hs
+++ Arithematic/p39.hs
@@ -2,7 +2,7 @@
 primesInRange :: Int -> Int -> [Int]
 primesInRange a b 
     | b < 3 = []
-    | a < 3 = [x | x <- [3..(b - 1)], isPrime x]
+    | a < 3 = [2] ++ [x | x <- [3..(b - 1)], isPrime x]
     | otherwise = [x | x <- [a..(b - 1)], isPrime x ]
         where
             isPrime n = null [x | x <- [2..(n-1)], n `mod` x == 0]
--- Misc/primesInRange.hs
+++ Misc/primesInRange.hs
@@ -2,6 +2,6 @@
 primesInRange :: Int -> [Int]
 primesInRange n 
     | n < 3 = []
-    | otherwise = [x | x <- [3..(n - 1)], isPrime x ]
+    | otherwise = [2] ++ [x | x <- [3..(n - 1)], isPrime x ]
         where
             isPrime n = null [x | x <- [2..(n-1)], n `mod` x == 0]
