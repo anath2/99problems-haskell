ddae455e5438cbab7b584ab65178d1ffa8c49c8c
Ajit Nath <nath.ajit01@gmail.com>
2017-12-09 17:18:45 +0800

    Generate a table of booleans

--- Logic and Codes/p46.hs
+++ Logic and Codes/p46.hs
@@ -1,1 +1,18 @@
--- Define predicate that prints the table
+-- Define built in boolean operations
+not' = not 
+and'= (&&)
+or' = (||)
+eq' = (==)
+
+-- Custom boolean operations
+nxor' a b  = foldr eq' True [a, b]
+xor' a b = not $ nxor' a b
+
+nand' a b = not' $ and' a b
+nor' a b = not' $ or' a b
+impl' a b = or' (not' a) b
+
+table :: (Bool -> Bool -> Bool) -> IO()
+table f = putStrLn $ concatMap (++ "\n") 
+            [show a ++ " " ++ show b ++ " " ++ show (f a b) 
+                | a <- [True, False], b <- [True, False]]
