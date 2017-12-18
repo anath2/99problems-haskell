bbb96f7e755ff4ad3018ca4b0bb82849911e5b06
Ajit Nath <nath.ajit01@gmail.com>
2017-12-19 01:27:11 +0800

    Balanced tree

--- Binary Trees/p55.hs
+++ Binary Trees/p55.hs
@@ -2,14 +2,30 @@
 --[WIP]
 -- Construct permutations of balanced binary trees from the number of nodes
 
-data Tree a = Empty | Branch a (Tree a) (Tree a)
-    deriving (Show, Eq)
+data Tree a = Empty | Branch a (Tree a) (Tree a) deriving (Show, Eq)
 
-cbTrees :: Int -> [Tree Char]
-cbTrees 0 = [Empty]
-
-cbalTree n = let (q, r) = (n - 1) `quotRem` 2
-    in [
-        Branch 'x' left right | i <- [q .. q + r], left  <- cbalTree i,
-        right <- cbalTree (n - i - 1)
-    ]
+leaf x = Branch x Empty Empty
+ 
+main = putStrLn $ concatMap (\t -> show t ++ "\n") balTrees
+    where balTrees = filter isBalancedTree (makeTrees 'x' 4)
+ 
+isBalancedTree :: Tree a -> Bool 
+isBlanacedTree Empty = True
+isBalancedTree (Branch _ l r) = abs (countBranches l - countBranches r) <= 1
+                                && isBalancedTree l && isBalancedTree r
+isBalancedTree _ = False
+ 
+countBranches :: Tree a -> Int
+countBranches Empty = 0
+countBranches (Branch _ l r) = 1 + countBranches l + countBranches r
+ 
+-- makes all possible trees filled with the given number of nodes
+makeTrees :: a -> Int -> [Tree a]
+makeTrees _ 0 = []
+makeTrees c 1 = [leaf c]
+makeTrees c n = lonly ++ ronly ++ landr
+    where lonly  = [Branch c t Empty | t <- smallerTree]
+          ronly = [Branch c Empty t | t <- smallerTree]
+          landr = concat [[Branch c l r | l <- fst lrtrees, r <- snd lrtrees] | lrtrees <- treeMinusTwo]
+          smallerTree = makeTrees c (n-1)
+          treeMinusTwo = [(makeTrees c num, makeTrees c (n-1-num)) | num <- [0..n-2]]
