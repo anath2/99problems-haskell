c4d779f96cab218dfcf4df60930e2647e1d7fb81
Ajit Nath <nath.ajit01@gmail.com>
2018-01-04 00:03:07 +0800

    collect tree leaves in a list

--- Binary Trees/p62.hs
+++ Binary Trees/p62.hs
@@ -1,1 +1,8 @@
 -- Collect leaves of a binary tree in a list
+data Tree a = Empty | Branch a (Tree a) (Tree a)
+collectLeaves :: (Tree a) -> [a]
+
+collectLeaves Empty = []
+collectLeaves Branch x Empty Empty = [x]
+collectLeaves Branch _ (Tree l) (Tree r) = collectLeaves l ++ collectLeaves r
+
