1d9460777ea6155e1f372ecf5d5af79d073aae22
Ajit Nath <nath.ajit01@gmail.com>
2018-01-09 23:56:59 +0800

    Collect elements at nth level of the tree

--- Binary Trees/p62b.hs
+++ Binary Trees/p62b.hs
@@ -4,3 +4,8 @@ data Tree a = Empty | Branch a (Tree a) (Tree a)
     deriving (Eq, Show)
 
 atLevel :: (Tree a) -> Int -> [a]
+atLevel Empty _ = []
+atLevel (Branch x l r) n 
+    | n < 1         = []
+    | n == 1        = [x]
+    | otherwise     = atLevel l (n - 1) ++ atLevel r (n - 1)
