02ae531168ca48483b2e71d2d81e4b17114f5f3f
Ajit Nath <nath.ajit01@gmail.com>
2018-01-21 00:37:02 +0800

    String representation of a tree

--- Binary Trees/p66.hs
+++ Binary Trees/p66.hs
@@ -1,1 +1,9 @@
 -- Generate a string representation of a binary treee
+data Tree a = Empty | Branch a (Tree a) (Tree a)
+
+treeToString :: Tree Char -> String
+-- Case : Empty tree
+treeToString Empty = ""
+-- Case : Tree with one node
+treeToString (Branch x (Empty) (Empty)) = [x]
+treeToString (Branch x l r) = x : '(' : ((treeToString l) ++ "," ++ (treeToString r) ++ ")")
