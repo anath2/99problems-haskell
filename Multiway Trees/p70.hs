d4da252c3bd44e78e99f4eb1233eb72c5d0f4b3e
Ajit Nath <nath.ajit01@gmail.com>
2018-01-31 00:36:44 +0800

    Multiway tree from string

--- Multiway Trees/p70.hs
+++ Multiway Trees/p70.hs
@@ -1,1 +1,15 @@
 -- Construct a tree from nodestring
+data Tree a = Node a [Tree a]
+
+stringToTree :: String -> Tree Char
+stringToTree (x:xs@(y:ys)) 
+     | y == '^'  = Node x []
+     | otherwise = Node x (map stringToTree subs)
+               where subs = snd $ foldl parse ([],[]) $ init xs
+	             parse ([],[])      z = ([z], [[z]])
+	             parse (stack, acc) z = (stack', acc')
+			       where stack' 
+			               | z == '^'  = init stack
+				       | otherwise = stack ++ [z]
+			             acc'   = if stack == []
+				                then acc ++ [[z]]
