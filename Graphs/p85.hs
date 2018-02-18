aafc715fc3983e44ceab5613a865617f1b56ae5b
Ajit Nath <nath.ajit01@gmail.com>
2018-02-18 23:38:04 +0800

    Canonical form of graph

--- Graphs/p85.hs
+++ Graphs/p85.hs
@@ -21,3 +21,16 @@ iso g@(Graph xs ys) h@(Graph xs' ys') = length xs == length xs' &&
                                         canon g == canon h
 
 canon :: (Ord a, Enum a) => Graph a -> String
+canon g = minimum $ map f $ perm $ length a
+    where
+        Adj a = graphToAdj g
+        v = map fst a
+        perm n = foldr (\x xs -> [i : s | i <- [1..n], s <- xs, i `notElem` s]) [[]] [1..n]
+        f p = let n = zip v p
+            in show [(snd x,
+                sort id $ map (\x ->
+                    snd $ head $ snd $ break ((==) x . fst) n) $ snd $ find a x)
+                | x <- sort snd n]
+        sort f n = foldr (\x xs -> let (lt, gt) = break ((<) (f x) . f) xs
+                                   in lt ++ [x] ++ gt) [] n
+        find a x = let (xs, ys) = break ((==) (fst x) . fst) a in head ys
