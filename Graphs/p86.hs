84001daca62a0b02543f76e31c15ac67f577ecfd
Ajit Nath <nath.ajit01@gmail.com>
2018-02-21 23:25:50 +0800

    Graph coloring [WIP]

--- Graphs/p86.hs
+++ Graphs/p86.hs
@@ -1,6 +1,21 @@
 -- Graph coloration using Welch Powell's algorithm
 
-data Graph a = Graph [a] [(a, a)]
-                deriving (Eq, Show)
+data Graph a        = Graph [a] [(a, a)]
+                        deriving (Eq, Show)
 
-data Adjacency a = Adj 
+data Adjacency a    = Adj [(a, [a])]
+                        deriving (Eq, Show)
+                        
+kcolor :: (Eq a, Ord a) => Graph a -> [(a, Int)]
+kcolor g = kcolor' x [] 1
+    where
+        Adj x = sortg g
+        kcolor' [] ys _ = ys
+        kcolor' xs ys n = let ys' = color xs ys n
+                        in kcolor' [x | x <- xs, notElem (fst x, n) ys']
+                    ys'
+                    (n + 1)
+        color []          ys n = ys
+        color ((v, e):xs) ys n = if any (\x -> (x, n) `elem` ys) e
+                                then color xs ys n
+                                else color xs ((v, n) : ys) n
