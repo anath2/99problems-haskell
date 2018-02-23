b5177bc08f43102c54f2d64fcc7decb6391ab39f
Ajit Nath <nath.ajit01@gmail.com>
2018-02-23 23:10:48 +0800

    Graph coloring

--- Graphs/p86.hs
+++ Graphs/p86.hs
@@ -23,3 +23,24 @@ kcolor g = kcolor' x [] 1
 -- determines chromatic number, given graph coloration
 chromatic :: [(a, Int)] -> Int
 chromatic x = length $ foldr (\(a, n) xs -> if n `elem` xs then xs else n : xs) [] x
+
+-- Graph to ajdacency representation
+graphToAdj :: (Eq a) => Graph a -> Adjacency a
+graphToAdj (Graph [] _)      = Adj []
+graphToAdj (Graph (x:xs) ys) = Adj ((x, ys >>= f) : zs)
+     where 
+        f (a, b) 
+            | a == x = [b]
+            | b == x = [a]
+            | otherwise = []
+        Adj zs = graphToAdj (Graph xs ys)
+
+-- Graph sorted by node degree
+sortg :: (Eq a, Ord a) => Graph a -> Adjacency a
+sortg g = Adj $ map (\(a, b) -> (a, sort b 1 maximum)) $ sort x 1 maxv
+    where
+        Adj x = graphToAdj g
+        sort [] _ _ = []
+        sort xs n f = let m = f xs in
+            m : sort [x | x <- xs, x /= m] (n + 1) f
+        maxv (x:xs) = foldr (\a@(a1, _) b@(b1, _) -> if a1 > b1 then a else b) x xs
