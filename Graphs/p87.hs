36d1a601c2a3e610ebbe45b8bb442585a647f356
Ajit Nath <nath.ajit01@gmail.com>
2018-02-26 01:46:57 +0800

    Depth first traversal

--- Graphs/p87.hs
+++ Graphs/p87.hs
@@ -5,3 +5,17 @@ type Edge = (Node, Node)
 type Graph = ([Node], [Edge])
 
 depthFirst :: Graph -> Node -> [Node]
+depthFirst (v, e) n 
+    | [ x | x <- v, x == n ] == [] = []
+    | otherwise = dfRec (v, e) [n]
+
+dfRec :: Graph  -> [Node] -> [Node]
+dfRec ([], _) _     = []
+dfRec (_, _) []     = []
+dfRec (v,e) (top:stack)
+    | [ x | x <- v, x == top ] == [] = dfRec (newv, e) stack
+    | otherwise = top : dfRec (newv, e) (adjacent ++ stack)
+    where
+        adjacent = [ x | (x,y) <- e, y == top] ++ [ x | (y,x) <- e, y == top ]
+        newv = [ x | x<-v, x /= top ]
+        
