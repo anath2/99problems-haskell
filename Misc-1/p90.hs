331085176b562bfa83551799a91ebf4732e7cf91
Ajit Nath <nath.ajit01@gmail.com>
2018-03-04 23:57:39 +0800

    Eight queen problem

--- Misc-1/p90.hs
+++ Misc-1/p90.hs
@@ -1,2 +1,9 @@
 -- Eight queen problem
 queens :: Int ->[[Int]]
+queens n = filter test (generate n)
+    where generate 0        = [[]]
+    generate k              = [q : qs | q <- [1..n], qs <- generate (k-1)]
+    test []                 = True
+    test (q:qs)             = isSafe q qs && test qs
+    isSafe try qs           = not (try `elem` qs || sameDiag try qs)
+    sameDiag try qs         = any (\(colDist,q) -> abs (try - q) == colDist) $ zip [1..] qs
