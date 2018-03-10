dd7a66bcbf546a8b5de40268931d5ba57370a441
Ajit Nath <nath.ajit01@gmail.com>
2018-03-10 23:11:13 +0800

    Generate regular simple graphs [WIP]

--- Misc-1/p94.hs
+++ Misc-1/p94.hs
@@ -6,3 +6,18 @@ data Adjacency a = Adj [(a, [a])]
                         deriving (Show, Eq)
 
 regular :: Int -> Int -> [Graph Int]
+regular n k 
+    | r == 1 || n <= k || n < 0 || k < 0 = []
+    | otherwise = map (adjToGraph . fst) $ 
+        foldr (\x xs -> if any ((==) (snd x) . snd) xs then xs else x : xs) [] $ 
+        zip a $ map canon a
+    
+    where
+        a = filter (\(Adj a) -> all ((==) k . length . snd) a) $
+            map (graphToAdj . Graph [1..n]) $ perm e q
+        e = map (\xs -> (head xs, last xs)) $ perm [1..n] 2
+        (q, r) = (n * k) `quotRem` 2
+        perm n k = foldr (\x xs -> 
+                        [i : s | i <- n, s <- xs, i `notElem` s, asc (i : s)])
+                        [[]] [1..k]
+        asc xs = all (uncurry (<)) $ zip xs $ tail xs
