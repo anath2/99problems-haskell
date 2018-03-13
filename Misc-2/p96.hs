c66c08fef2041c1cdc4ff93c2c45cae3124ae35b
Ajit Nath <nath.ajit01@gmail.com>
2018-03-13 23:49:47 +0800

    Syntax diagram to recursive structure

---
+++ Misc-2/p96.hs
@@ -0,0 +1,7 @@
+-- Transform the syntax diagram given in problem 
+-- 96 to a system of syntax diagrams that do not
+-- contain work
+
+import Data.Char
+syntaxCheck :: String -> Bool
+syntaxCheck []      = False
